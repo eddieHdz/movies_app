ed95e95e1eab4d21c5b8782e7ad873536675cd74
eduardo <eduardo.hdzdelarosa@gmail.com>
2022-01-03 12:49:48 -0700

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/screens/screens.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Movies',
+      initialRoute: 'home',
+      routes: {'home': (_) => HomeScreen(), 'details': (_) => DetailsScreen()},
+    );
+  }
+}
